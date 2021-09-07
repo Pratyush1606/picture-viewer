f8f89a88a9e65744c66f070f7ec972c90e37cd13
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2021-09-07 20:54:23 +0530

    Added pic_app

---
+++ pic_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from pic_app import views
+
+app_name = 'pic_app'
+urlpatterns = [
+    
+]
--- picture_viewer/settings.py
+++ picture_viewer/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%a37a2uf4oz8h=bue_7)7p_&r1tqr&dlh7+-3r+*h)+y)@p9!u'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'pic_app.apps.PicAppConfig',
 ]
 
 MIDDLEWARE = [
